190894356a0af4a5300a63591de098c92473d19a
Catherine Smith <catsmith24601@gmail.com>
2021-03-18 12:15:04 -0400

    set up basic html with 4 placeholder answers per question, filled in basic css

---
+++ Assets/script.js
@@ -0,0 +1,26 @@
+var startButton = document.getElementById("#start-btn");
+var startButton = document.getElementById("#next-btn");
+var startButton = document.getElementById("#answer-buttons");
+var startButton = document.getElementById("#question");
+
+
+
+
+
+
+
+function startGame () {
+
+} 
+
+
+function setNextQuestion() {
+    
+}
+
+
+function selectAnswer() {
+
+}
+
+
